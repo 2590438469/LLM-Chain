633daad7892e50c347a06e11e5bca4454a6a7eb0
2590438469 <107899363+2590438469@users.noreply.github.com>
2024-12-03 15:09:44 +0800

    Update

--- dataset/maxContract/2094/0x01c54b1c806d0ccee805bdb7689a544917abe608.sol
+++ dataset/maxContract/2094/0x01c54b1c806d0ccee805bdb7689a544917abe608.sol
@@ -14,7 +14,7 @@ library SafeMath {
 		assert(c / a == b);
 		return c;
 	}
-    
+      
 	function div(uint256 a, uint256 b) internal pure returns (uint256) {
 		// assert(b > 0); // Solidity automatically throws when dividing by 0
 		uint256 c = a / b;
